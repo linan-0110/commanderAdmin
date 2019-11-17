364c2ebedf1fda959a695b9e0bc2ac5bd4c0eed7
linan-0110 <1597711363@qq.com>
2019-11-17 22:47:11 +0800

    uodate route lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/page/Login/Login'; //登录页
-import home from '@/page/home/home'; // 主页
-import myAccount from '@/page/myAccount/myAccount'; // 我的账号
-import myEarnings from '@/page/myEarnings/myEarnings'; // 我的收益
-import carryCash from '@/page/carryCash/carryCash'; // 提现
-import carryCashSuccess from '@/page/carryCash/carryCashSuccess/carryCashSuccess'; // 提现提交
-import alterBankCard from '@/page/carryCash/alterBankCard/alterBankCard'; // 更改银行卡
-import fullStatus from '@/page/fullStatus/fullStatus'; //整体状况
-import areaOrder from '@/page/areaOrder/areaOrder'; //小区订单
-import orderExpress from '@/page/orderExpress/orderExpress'; //订单配送
-import confirmDelivery from '@/page/orderExpress/confirmDelivery/confirmDelivery'; // 订单配送
-import clientGetCargo from '@/page/clientGetCargo/clientGetCargo'; // 顾客提货
-import myOrder from '@/page/myOrder/myOrder'; // 我的订单
-import discountCoupon from '@/page/discountCoupon/discountCoupon'; // 优惠劵
-import myAgreement from '@/page/myAgreement/myAgreement'; // 我的协议
+
+const Login = () => import('@/page/Login/Login');  // 登录页
+const home = () => import('@/page/home/home'); // 主页
+const myAccount = () => import('@/page/myAccount/myAccount'); // 我的账号
+const myEarnings = () => import('@/page/myEarnings/myEarnings'); // 我的收益
+const carryCash = () => import('@/page/carryCash/carryCash'); // 提现
+const carryCashSuccess = () => import('@/page/carryCash/carryCashSuccess/carryCashSuccess');  // 提现提交
+const alterBankCard = () => import('@/page/carryCash/alterBankCard/alterBankCard'); // 更改银行卡
+const fullStatus = () => import('@/page/fullStatus/fullStatus'); // 整体状况
+const areaOrder = () => import('@/page/areaOrder/areaOrder'); // 小区订单
+const orderExpress = () => import('@/page/orderExpress/orderExpress'); // 订单配送
+const confirmDelivery = () => import('@/page/orderExpress/confirmDelivery/confirmDelivery'); // 确认提货
+const clientGetCargo = () => import('@/page/clientGetCargo/clientGetCargo');  // 顾客提货
+const myOrder = () => import('@/page/myOrder/myOrder'); // 我的订单
+const discountCoupon = () => import('@/page/discountCoupon/discountCoupon');  // 优惠劵
+const myAgreement = () => import('@/page/myAgreement/myAgreement'); // 我的协议
 
 Vue.use(Router);
 
