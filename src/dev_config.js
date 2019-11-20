6aeb5c1af5b44a0b76f78c1b6d66671bf70256ce
linan-0110 <1597711363@qq.com>
2019-11-20 22:42:51 +0800

    .

--- src/dev_config.js
+++ src/dev_config.js
@@ -1,7 +1,7 @@
 export default {
-    ONLINE_BASE_HREF: "https://mall.jjsqwg.com/mobile/index.html#/", // 服务器线上根地址 
+    ONLINE_BASE_HREF: "https://mall.jjsqwg.com/mobile/index.html#/", // 正式 线上根地址 
     SERVER_HREF: "https://allapi.jjsqzg.com/Salors" //  正式 服务器地址
-    
-    // ONLINE_BASE_HREF: "http://localhost:8081/#/", // 本机线上根地址 
+
+    // ONLINE_BASE_HREF: "http://localhost:8081/#/", // 本机 线上根地址 
     // SERVER_HREF: "https://allinone.jjsqwg.com/Salors" // 测试 服务器地址 
 }
