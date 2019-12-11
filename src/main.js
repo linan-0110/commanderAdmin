fffb1da957013694cfda9f56ae163766c6d2a6a6
linan-0110 <1597711363@qq.com>
2019-12-11 10:10:33 +0800

    把两个公共样式文件移动到了src/publicStyle下

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import '../static/init.css';
-import '../static/public.css';
+import '@/publicStyle/init.css';
+import '@/publicStyle/public.css';
 
 
 /* vant start */
